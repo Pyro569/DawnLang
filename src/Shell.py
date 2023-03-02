24e9b93ca5ca215a936ece281ec168d135d6c788
Pyro <79991107+Pyro569@users.noreply.github.com>
2023-03-01 21:32:53 -0500

    Add files via upload

--- src/Shell.py
+++ src/Shell.py
@@ -1,8 +1,11 @@
 import DawnLang
+import readline
 
 while True:
     command = input('DawnLang > ')
     result, error = DawnLang.run('<stdin>', command)
-    
-    if error: print(error.turnString())
-    else: print(result)
+
+    if error:
+        print(error.as_string())
+    else:
+        print(result)
