3d1d463fa97e905d17f1d3a3e5be81495254d179
Pyro <doggolover98@outlook.com>
2023-03-07 16:39:32 -0500

    String support (add docs later)

--- src/DawnLang.py
+++ src/DawnLang.py
@@ -96,6 +96,7 @@ class Position:
 
 TT_INT = 'INT'
 TT_FLOAT = 'FLOAT'
+TT_STRING = 'STRING'
 TT_IDENTIFIER = 'IDENTIFIER'
 TT_KEYWORD = 'KEYWORD'
 TT_PLUS = 'PLUS'
@@ -180,6 +181,8 @@ class Lexer:
                 tokens.append(self.make_number())
             elif self.current_char in LETTERS:
                 tokens.append(self.make_identifier())
+            elif self.current_char == '"':
+                tokens.append(self.make_string())
             elif self.current_char == '+':
                 tokens.append(Token(TT_PLUS, pos_start=self.pos))
                 self.advance()
@@ -240,6 +243,31 @@ class Lexer:
             return Token(TT_INT, int(num_str), pos_start, self.pos)
         else:
             return Token(TT_FLOAT, float(num_str), pos_start, self.pos)
+
+    def make_string(self):
+        string = ''
+        pos_start = self.pos.copy()
+        escape_character = False
+        self.advance()
+        
+        escape_characters = {
+            'n': '\n',
+            't': '\t'
+        }
+        
+        while self.current_char != None and self.current_char != '"' or escape_character:
+            if escape_character:
+                string += escape_characters.get(self.current_char, self.current_char)
+            else:
+                if self.current_char == '\\':
+                    escape_character = True
+                else:
+                    string += self.current_char
+            self.advance()
+            escape_character = False
+            
+        self.advance()
+        return Token(TT_STRING, string, pos_start, self.pos)
 
     def make_identifier(self):
         id_str = ''
@@ -309,6 +337,16 @@ class Lexer:
 
 
 # NODES
+
+class StringNode:
+    def __init__(self, tok):
+        self.tok = tok
+
+        self.pos_start = self.tok.pos_start
+        self.pos_end = self.tok.pos_end
+
+    def __repr__(self):
+        return f'{self.tok}'
 
 class NumberNode:
     def __init__(self, tok):
@@ -686,6 +724,11 @@ class Parser:
             res.register_advancement()
             self.advance()
             return res.success(NumberNode(tok))
+        
+        if tok.type in TT_STRING:
+            res.register_advancement()
+            self.advance()
+            return res.success(StringNode(tok))
 
         elif tok.type == TT_IDENTIFIER:
             res.register_advancement()
@@ -1115,6 +1158,36 @@ class Number(Value):
 
     def __repr__(self):
         return str(self.value)
+    
+class String(Value):
+    def __init__(self, value):
+        super().__init__()
+        self.value = value
+        
+    def added_to(self, other):
+        if isinstance(other, String):
+            return String(self.value + other.value).set_context(self.context), None
+        else:
+            return None, Value.illegal_operation(self, other)
+        
+    def multed_by(self, other):
+        if isinstance(other, Number):
+            return String(self.value * other.value).set_context(self.context), None
+        else:
+            return None, Value.illegal_operation(self.other)
+        
+    def is_true(self):
+        return len(self.value) > 0
+    
+    def copy(self):
+        copy = String(self.value)
+        copy.set_pos(self.pos_start, self.pos_end)
+        copy.set_context(self.context)
+        return copy
+    
+    def __repr__(self):
+        return f'"{self.value}"'
+        
 
 class Function(Value):
     def __init__(self, name, body_node, arg_names):
@@ -1210,6 +1283,11 @@ class Interpreter:
             Number(node.tok.value).set_context(
                 context).set_pos(node.pos_start, node.pos_end)
         )
+        
+    def visit_StringNode(self, node, context):
+        return RTResult().success(
+			String(node.tok.value).set_context(context).set_pos(node.pos_start, node.pos_end)
+		)
 
     def visit_VarAccessNode(self, node, context):
         res = RTResult()
